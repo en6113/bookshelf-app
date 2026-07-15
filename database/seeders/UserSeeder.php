e03b56878e6f96b9b0361a5f191c6e95b0381621
en6113 <mitayan3@gmail.com>
2026-07-15 13:13:18 +0900

    refactor/user-seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,35 +14,18 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users = [
-            [
-                'name' => '山田太郎',
-                'email' => 'yamada@example.com',
-                'password' => Hash::make('password'),
-            ],
-            [
-                'name' => '鈴木花子',
-                'email' => 'suzuki@example.com',
-                'password' => Hash::make('password'),
-            ],
-            [
-                'name' => '田中一郎',
-                'email' => 'tanaka@example.com',
-                'password' => Hash::make('password'),
-            ],
-            [
-                'name' => '佐藤美咲',
-                'email' => 'sato@example.com',
-                'password' => Hash::make('password'),
-            ],
-            [
-                'name' => '高橋健太',
-                'email' => 'takahashi@example.com',
-                'password' => Hash::make('password'),
-            ],
+            ['name' => '山田太郎','email' => 'yamada@example.com'],
+            ['name' => '鈴木花子','email' => 'suzuki@example.com'],
+            ['name' => '田中一郎','email' => 'tanaka@example.com'],
+            ['name' => '佐藤美咲','email' => 'sato@example.com'],
+            ['name' => '高橋健太','email' => 'takahashi@example.com'],
         ];
 
         foreach ($users as $user) {
-            User::firstOrCreate($user);
+            User::firstOrCreate(['email' => $user['email']],[
+                'name' => $user['name'],
+                'password' => Hash::make('password'),
+            ]);
         }
     }
 }
